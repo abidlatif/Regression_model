c256bc97972336428e313d97659f2debf0345c0e
Abid Latif <imabidlatif@gmail.com>
2024-02-14 13:45:41 +0100

    variables changed

---
+++ Excersize 7.R
@@ -0,0 +1,9 @@
+#Opening the data already provided
+library("MASS")
+attach(Cars93)
+mydata=data.frame(Price, EngineSize,
+                  RPM, Horsepower, Weight, Cylinders, MPG.city, Passengers,
+                  MPG.highway)
+
+mymodel= lm(Price ~ MPG.city)
+mymodel2= lm(Price ~ MPG.highway)
