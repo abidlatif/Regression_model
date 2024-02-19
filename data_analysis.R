de5ba883d98417507fd7cf35552de19ffdeb2f7b
Abid Latif <imabidlatif@gmail.com>
2024-02-19 23:49:30 +0100

    data classification

--- data_analysis.R
+++ data_analysis.R
@@ -35,3 +35,19 @@ barplot(room_frequency,main = "Bar plot", xlab = "number of rooms", ylab = "freq
 
 rel_freq= room_frequency/length(number_of_rooms)
 pie(rel_freq)
+
+
+age=c(18,19,21,28,23,29,33,31,31,30,39,44,42,22,35,21,23,45,8,45,13,32,31)
+gender=c(1,1,1,2,2,1,2,2,1,2,1,1,1,1,1,2,1,2,2,2,1,1,2)
+degree=c(1,2,2,4,4,1,1,3,2,5,5,1,2,3,4,4,1,2,3,4,4,2,1)
+stay=c(0,2,3,9,11,1,2,3,2,14,12,11,8,8,6,6,5,5,5,5,6,2,3)
+diagnosis=c(2,3,1,1,2,2,2,1,2,2,2,4,3,1,2,2,2,3,4,2,3,1,1)
+
+hospital_data= data.frame(age, gender, degree, stay, diagnosis)
+dim((hospital_data))
+plot(age)
+pie(gender)
+gender_freq=table(gender)
+pie(gender_freq/length(gender))
+degree_freq=table(degree)
+pie(degree_freq/length(degree))
