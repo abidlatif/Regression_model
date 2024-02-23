87ee4a90c53f03c358105285d6f12c4f71db38f6
Abid Latif <imabidlatif@gmail.com>
2024-02-23 01:03:26 +0100

    confidence interval

--- observations.R
+++ observations.R
@@ -5,4 +5,9 @@ sample_data2 <- rnorm(100, mean = 5.1, sd=2.2)
 ks_result= ks.test(sample_data, sample_data2)
 print(ks_result)
 
+# Question two 
+x = rnorm(100, mean = 10, sd=4.7)
 
+y= rnorm(100, mean = 10.8, sd= 5)
+
+ks.test(x,y)
