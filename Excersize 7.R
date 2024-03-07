5f918945041aca81ab9d710d0f3cd0129b60ec26
Abid Latif <imabidlatif@gmail.com>
2024-03-07 23:49:30 +0100

    model updated

--- Excersize 7.R
+++ Excersize 7.R
@@ -1,6 +1,7 @@
 #Opening the data already provided
 library("MASS")
 attach(Cars93)
+#Variables assignment 
 mydata=data.frame(Price, EngineSize,
                   RPM, Horsepower, Weight, Cylinders, MPG.city, Passengers,
                   MPG.highway)
@@ -9,5 +10,4 @@ mymodel= lm(Price ~ MPG.city)
 mymodel2= lm(Price ~ MPG.highway)
 mymodel3= lm(Price ~ MPG.highway, MPG.city)
 plot(lm(Price ~ MPG.city))
-
- 
+#
