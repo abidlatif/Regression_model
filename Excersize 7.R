715ce139d4a09dbcbca2415dfbb68dbf7626d16e
Abid Latif <imabidlatif@gmail.com>
2024-03-08 23:56:47 +0100

    model updated

--- Excersize 7.R
+++ Excersize 7.R
@@ -5,7 +5,7 @@ attach(Cars93)
 mydata=data.frame(Price, EngineSize,
                   RPM, Horsepower, Weight, Cylinders, MPG.city, Passengers,
                   MPG.highway)
-
+#setting the model 
 mymodel= lm(Price ~ MPG.city)
 mymodel2= lm(Price ~ MPG.highway)
 mymodel3= lm(Price ~ MPG.highway, MPG.city)
