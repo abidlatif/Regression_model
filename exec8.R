7d0cf64b414cf9b1659afd0bb65290e16a996dda
Abid Latif <imabidlatif@gmail.com>
2024-03-16 23:27:25 +0100

    rew means

--- exec8.R
+++ exec8.R
@@ -3,6 +3,7 @@
 statistics = read.csv(file="statistics.csv")
 names(statistics)
 attach(statistics)
+#row means
 statistics.joy_mean1 = rowMeans(subset(statistics,
                                        select=c(stats_joy1, stats_joy2, stats_joy3, stats_joy4,
                                                 stats_joy5)), na.rm=TRUE)
