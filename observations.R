b30ba4cf257df5e25b888f80a74d484456f346c0
Abid Latif <imabidlatif@gmail.com>
2024-03-29 23:52:05 +0100

    ks test

--- observations.R
+++ observations.R
@@ -4,6 +4,8 @@ sample_data <- rnorm(100, mean = 5, sd=2)
 sample_data2 <- rnorm(100, mean = 5.1, sd=2.2)
 #k s test 
 ks_result= ks.test(sample_data, sample_data2)
+#print
+
 print(ks_result)
 
 # Question two 
